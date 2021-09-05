33e80861a98703a6ac507ef9b62a45151a12c5b5
304089 <9457imai2@gmail.com>
2021-09-05 03:57:27 +0000

    検索機能完成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,9 +5,9 @@ class PostsController < ApplicationController
     end
 
     def index
-      @posts = Post.all
+      @q = Post.ransack(params[:q])
+      @posts = @q.result(distinic: true)
     end
-
 
     def show
       @post = Post.find(params[:id])
